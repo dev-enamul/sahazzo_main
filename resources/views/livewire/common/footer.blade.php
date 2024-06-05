097a4d3fa484cdb001f4efe4b82eee5edd727275
Md Enamul Haque <enamul807849@gmail.com>
2024-06-05 18:32:26 +0600

    update footer section

--- resources/views/livewire/common/footer.blade.php
+++ resources/views/livewire/common/footer.blade.php
@@ -38,25 +38,6 @@
                 </div>
                 <div class="footer-widget col-sm-6 col-md-6 col-lg-4">
                     <h3>Newsletter Registration</h3>
-                    <p>Subscribe today to receive the latest <strong>Corpboot</strong> news via email. You may unsubscribe from this service at any time.</p>
-                    <!-- Newsletter Form-->
-                    <form method="get">
-                        <div class="input-group newsletter">
-                            <label class="sr-only" for="subscribe-email">Enter your email...</label>
-                            <input type="email" class="form-control" id="subscribe-email" placeholder="Enter your email...">
-                            <span class="input-group-btn">
-                                <button type="submit" class="btn"><i class="fa fa-paper-plane-o"></i></button>
-                            </span>
-                        </div>
-                    </form> 
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="footer-bottom">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
                     <ul class="list-inline social">
                         <li class="list-inline-item">
                             <a target="blank" href="{{$contact->fb}}">
@@ -77,16 +58,10 @@
                             <a target="blank" href="{{$contact->youtube}}">
                                 <i class="fa fa-youtube"></i>
                             </a>
-                        </li>
-                        <!-- <li class="list-inline-item">
-                            <a href="{{$contact->youtube}}">
-                                <i class="fa fa-instagram"></i>
-                            </a>
-                        </li> -->
-                    </ul>
-                </div>
-                <div class="col-sm-6 credits">
-                    <p>&#xA9; Marse Planing And Engineering. All Rights Reserved.</p>
+                        </li> 
+                    </ul> 
+
+                    <p class="mt-4">&#xA9; Marse Planing And Engineering. All Rights Reserved.</p>
                     <p class="small">
                         <i class="fa fa-angle-right"></i>
                         Developed by: <a href="#">Shahazzo Software</a>
@@ -95,6 +70,7 @@
             </div>
         </div>
     </div>
+    
     <!--WhatsApp Button-->
     <a target="blank" href="https://api.whatsapp.com/send?phone={{$contact->whatsapp}}&amp;text=hello" class="whatsappButton"
         id="whatsappButton"><i class="fa fa-whatsapp"></i></a>
